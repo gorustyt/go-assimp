6e678f85c29e8643470bd44cf59c1204accbe03e
YangT <1228231049@qq.com>
2023-12-19 23:39:38 +0800

    修改主文件â

--- assimp.go
+++ assimp.go
@@ -1,10 +1,14 @@
 package awesomeProject2
 
+import (
+	"assimp/core"
+	"assimp/driver"
+)
+
 // AiImportFile Reads the given file and returns its content.
-func AiImportFile(path string, pFlags int) error {
-	return AiImportFileExWithProperties(path, pFlags, nil)
-}
-
-func AiImportFileExWithProperties(path string, pFlags int, props []AiPropertyStore) error {
-
+func ParseFile(path string) (*core.AiScene, error) {
+	im := driver.NewImporter()
+	s, err := im.ReadFile(path, 0)
+	im = nil
+	return s, err
 }
